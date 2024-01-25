3bf48f0dfa74e1a431bd49aafc1f9f57c7276238
fgurbano23 <fgurbano23@gmail.com>
2024-01-25 07:39:28 -0400

    .

--- src/app/modules/reports/create-report/create-report.component.ts
+++ src/app/modules/reports/create-report/create-report.component.ts
@@ -12,9 +12,17 @@ import { CreateReportFormInterface } from './models/create-report-form.interface
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardContent } from '@angular/material/card';
 
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import {
+  Camera,
+  CameraPhoto,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
 import { BehaviorSubject } from 'rxjs';
 import { AsyncPipe, JsonPipe, NgOptimizedImage } from '@angular/common';
+import { CapacitorPlatform } from '@capacitor/core/types/platforms';
+import { Platform } from '@angular/cdk/platform';
 
 @Component({
   selector: 'app-create-report',
@@ -41,20 +49,41 @@ export class CreateReportComponent {
   image$ = this.image.asObservable();
 
   data: any = null;
+  error: any = null;
+
   form: FormGroup<CreateReportFormInterface> =
     this.fb.group<CreateReportFormInterface>({
       issue: new FormControl('', { validators: [Validators.required] }),
       description: new FormControl('', { validators: [Validators.required] }),
     });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private platform: Platform,
+  ) {}
 
   async take() {
-    Camera.getPhoto({
-      source: CameraSource.Prompt,
-      resultType: CameraResultType.DataUrl,
-    }).then((res) => {
-      this.data = res;
+    try {
+      const photo = await Camera.getPhoto({
+        source: CameraSource.Camera,
+        resultType: CameraResultType.Uri,
+      });
+      if (photo) {
+        const res = await fetch(photo.webPath!);
+        const blob = await res.blob();
+        const b64 = await this.blobToBase64(blob);
+        this.data = b64;
+      }
+    } catch (e) {
+      this.error = e;
+    }
+  }
+
+  blobToBase64(blob: Blob) {
+    return new Promise((resolve, _) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
     });
   }
 }
